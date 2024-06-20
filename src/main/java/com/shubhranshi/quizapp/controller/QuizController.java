976902ccf0dd71eadae431e09027c043d9a29f43
shubhranshii <shubhranshiagarwal@gmail.com>
2024-06-20 14:56:20 +0530

    added scoring logic

--- src/main/java/com/shubhranshi/quizapp/controller/QuizController.java
+++ src/main/java/com/shubhranshi/quizapp/controller/QuizController.java
@@ -1,6 +1,7 @@
 package com.shubhranshi.quizapp.controller;
 
 import com.shubhranshi.quizapp.model.QuestionWrapper;
+import com.shubhranshi.quizapp.model.Response;
 import com.shubhranshi.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +24,10 @@ public class QuizController {
     @GetMapping("/get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
         return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("/submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
     }
 }
--- src/main/java/com/shubhranshi/quizapp/service/QuizService.java
+++ src/main/java/com/shubhranshi/quizapp/service/QuizService.java
@@ -5,6 +5,7 @@ import com.shubhranshi.quizapp.dao.QuizDao;
 import com.shubhranshi.quizapp.model.Question;
 import com.shubhranshi.quizapp.model.QuestionWrapper;
 import com.shubhranshi.quizapp.model.Quiz;
+import com.shubhranshi.quizapp.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,5 +43,18 @@ public class QuizService {
             questionsForUser.add(qw);
         }
         return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz= quizDao.findById(id).get();
+        List<Question> questions= quiz.getQuestions();
+        int right=0;
+        int i=0;
+        for(Response response: responses){
+            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
+                right++;
+            i++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
